be1024bc6544b1d693ad487af5d1cb150caabb56
t-umeda <t-umeda@bbsakura.net>
2024-04-04 18:52:06 +0900

    fix: If the connection fails, set the check to false
    delete unnecessary checks

--- example/air-ulr-stress.js
+++ example/air-ulr-stress.js
@@ -1,3 +1,6 @@
+/*
+example stress test for AIR and ULR
+*/
 import { check } from 'k6';
 import diameter from 'k6/x/diameter';
 
@@ -7,42 +10,56 @@ export default function () {
     if (client == null) {
         client = new diameter.K6DiameterClient();
     }
-    client.connect({
-        addr: `127.0.0.1:3868`,
-        host: "magma-oai.openair4G.eur",
-        realm: "openair4G.eur",
-        network_type: "sctp",
-        retries: 0,
-        vendor_id: 10415,
-    });
-    for (let i = 0; i < 4096; i++) {
-        const airRes = client.checkSendAIR({
+    try {
+        const result = client.connect({
+            addr: `127.0.0.1:3868`,
+            host: "magma-oai.openair4G.eur",
+            realm: "openair4G.eur",
+            network_type: "sctp",
+            retries: 0,
             vendor_id: 10415,
-            app_id: 16777251,
-            ueimsi: "00" + (1010000000001 + i),
-            plmn_id: "\x05",
-            vectors: 3,
-            completion_sleep: 5,
         });
-        if (!airRes) {
+        check(result, {
+            'Connected': (result) => result == true
+        })
+    } catch (error) {
+        check(null, {
+            'Connected': false,
+        });
+        return;
+    }
+    for (let i = 0; i < 4096; i++) {
+        try {
+            const airRes = client.checkSendAIR({
+                ueimsi: "00" + (1010000000001 + i),
+                plmn_id: "\x05",
+                vectors: 3,
+                completion_sleep: 5,
+            });
             check(airRes, {
                 'Received AIR Response': (airRes) => airRes == true,
-                'Received ULR Response': false,
             });
-            continue;
+        } catch (error) {
+            check(null, {
+                'Received AIR Response': false,
+            });
         }
-        const ulrRes = client.checkSendULR({
-            vendor_id: 10415,
-            app_id: 16777251,
-            ueimsi: "00" + (1010000000001 + i),
-            plmn_id: "\x05",
-            vectors: 3,
-            completion_sleep: 5,
-        });
-        check(ulrRes, {
-            'Received AIR Response': (airRes) => airRes == true,
-            'Received ULR Response': (ulrRes) => ulrRes == true,
-        });
+        try {
+            const ulrRes = client.checkSendULR({
+                ueimsi: "00" + (1010000000001 + i),
+                plmn_id: "\x05",
+                vectors: 3,
+                completion_sleep: 5,
+            });
+            check(ulrRes, {
+                'Received ULR Response': (ulrRes) => ulrRes == true
+            });
+        } catch (error) {
+
+            check(null, {
+                'Received ULR Response': false
+            });
+        }
     }
     client.close();
 }
